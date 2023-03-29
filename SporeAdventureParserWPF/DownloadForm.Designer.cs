05586d29ed65280ed91dc6a9df23f2397b167a6a
RedGrox <ryzijbarsik95@gmail.com>
2023-03-29 22:08:14 +0300

    Add download window

---
+++ SporeAdventureParserWPF/DownloadForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SporeAdventureParserWPF
+{
+    partial class DownloadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DownloadForm));
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(12, 48);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(342, 33);
+            this.progBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progBar.TabIndex = 0;
+            this.progBar.UseWaitCursor = true;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusLabel.Location = new System.Drawing.Point(12, 19);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(155, 16);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Чтение приключения...";
+            this.statusLabel.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Мда, прогрессбар на WinForms...";
+            this.label1.UseWaitCursor = true;
+            // 
+            // DownloadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 115);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DownloadForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Пожалуйста, подождите...";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SporeAdventureParserWPF/DownloadForm.cs
@@ -0,0 +1,93 @@
+﻿using SporeApi.Creations;
+using System;
+using System.Windows.Forms;
+
+namespace SporeAdventureParserWPF
+{
+    public partial class DownloadForm : Form
+    {
+        public DownloadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadCreation(Creation creation)
+        {
+            statusLabel.Text = "Загрузка творения: " + creation.Name;
+            statusLabel.Refresh();
+            CreationDownloader.Download(creation);
+            progBar.Value++;
+        }
+        public void DownloadAdventure(params string[] uris)
+        {
+            /*
+             * Наитупейшее решение,
+             * но ничего лучше я придумать не смог...
+             */
+
+            Show();
+            Refresh();
+
+            bool notError = false;
+            foreach (string uri in uris)
+            {
+                notError = true;
+                statusLabel.Text = "Чтение приключения...";
+                progBar.Value = 0;
+                statusLabel.Refresh();
+
+                try
+                {
+                    Adventure adventure = new Adventure(Creation.Parse(uri));
+                    progBar.Maximum = 1 + adventure.AssetsCount;
+                    DownloadCreation(adventure);
+
+                    for (int i = 0; i < adventure.AssetsCount; i++)
+                        DownloadCreation(adventure.GetCreationFromAssetAt(i));
+                }
+                catch (Exception ex)
+                {
+                    notError = false;
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            //await Task.Run(() =>
+            //{
+            //    foreach (string uri in uris)
+            //    {
+            //        notError = true;
+            //        statusLabel.Text = "Чтение приключения...";
+            //        progBar.Value = 0;
+            //        //statusLabel.Refresh();
+
+            //        try
+            //        {
+            //            Adventure adventure = new Adventure(Creation.Parse(uri));
+            //            progBar.Maximum = 1 + adventure.AssetsCount;
+            //            DownloadCreation(adventure);
+
+            //            for (int i = 0; i < adventure.AssetsCount; i++)
+            //                DownloadCreation(adventure.GetCreationFromAssetAt(i));
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            notError = false;
+            //            MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        }
+            //    }
+            //});
+
+            Close();
+            if (notError)
+            {
+                string message = "Приключени";
+                if (uris.Length == 1)
+                    message += 'е';
+                else
+                    message += 'я';
+                message += " и творения успешно загружены!";
+                MessageBox.Show(message, "Готово!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
